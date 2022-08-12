a0b4ab2f5badcb91ff6584e87478e7b73e17205f
Yaqub Tawab Olamilekan <maktee2002@gmail.com>
2022-08-12 23:42:10 +0100

    day10

---
+++ day10/js10.js
@@ -0,0 +1,27 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+let lastChecked;
+function handlecheck(e) {
+  //check if the shift key is included and if they are checking it
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    // go ahead and do what we please
+    // loop over every single checkbox
+    checkboxes.forEach(checkbox => {
+      console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log('Starting to check them in between!');
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+
+  lastChecked = this;
+}
+
+checkboxes.forEach(element => {
+  element.addEventListener("click", handlecheck);
+});
