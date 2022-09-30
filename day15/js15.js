0e43b734d0f1e972325ebedff6b8d90dcae99dbd
Yaqub Tawab Olamilekan <maktee2002@gmail.com>
2022-09-30 22:04:56 +0100

    vibes

--- day15/js15.js
+++ day15/js15.js
@@ -1,27 +1,29 @@
 const addItems = document.querySelector('.add-items');
 const itemslist = document.querySelector('.plates');
 const items = [];
-
+  
 function addItem (e) {
     e.preventDefault();
-    const text = (this.querySelector('[name=item')).value
+    const text = (this.querySelector('[name=item]')).value ;
     const item = {
         text ,
         done : false
-    }
-    items.push(item)
-    copulatelist(items,itemslist)
+    };
+    items.push(item);
+    pulatelists(items , itemslist)
     this.reset();
+    console.log(this.undefined);
 }
 
-function copulatelist(plates = [] , placelists) {
-    placelists.innerHTML = plates.map((plate , i) => {
-        return `
-           <li> 
-             <label for = "">${plate.text}</label>
-           </li>
-        `;
-    }).join('');
-
+function pulatelists(plates = [] , platelists) {
+   platelists.innerHTML = plates.map((plate,i) => {
+      return `  
+      <li>
+        <input type="checkbox" data-index=${i} id=item${i} />
+        <label for ="item${i}">${plate.text}</label>
+      </li>
+      `
+   }).join('');
 }
-addItems.addEventListener('submit' , addItem)
+
+addItems.addEventListener('submit' , addItem);
